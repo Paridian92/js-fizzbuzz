e5d964ab28898fcf2dfe4ecbd2fe6a8d780c8519
Paridian92 <paridian92@hotmail.it>
2021-04-09 18:13:47 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 var numArray = []
 
-
-
 for (var i = 1; i<= 100 ; i++){
-  numArray.push(i)  
+    
+  if((i%3 ===0) && (i%5 === 0)){
+
+    numArray.push("fizzbuzz")
+  }else if(i%5 === 0){
+
+   numArray.push("buzz")
+  }else if(i%3 === 0){
+
+    numArray.push("fizz")
   
-if(i%3 === 0){
-  
-  numArray.push("fizz")
-  
-}else if(i%5 === 0){
-  numArray.push("buzz")
- 
-}else if((i%3 === 0) && (i%5 === 0)){
-  numArray.push("fizzbuzz")
- }
+  }else{
+    numArray.push(i)
+  }
 }
+
 console.log(numArray)
+
+
 
