f604ea609b3cdc7d2d5a3d79d7b9eab38c367af2
Paridian92 <paridian92@hotmail.it>
2021-04-09 17:36:01 +0200

    bloccato

--- js/script.js
+++ js/script.js
@@ -5,14 +5,16 @@ var numArray = []
 for (var i = 1; i<= 100 ; i++){
   numArray.push(i)  
   
-if(i%3){
-  numArray = "fizz"
-  }
+if(i%3 === 0){
+  
+  numArray.push("fizz")
+  
+}else if(i%5 === 0){
+  numArray.push("buzz")
+ 
+}else if((i%3 === 0) && (i%5 === 0)){
+  numArray.push("fizzbuzz")
+ }
 }
-
-
-
-
-
 console.log(numArray)
 
